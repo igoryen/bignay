6470fbbf140178610736567dc2161ec3a51f5c90
igoryen <igor.yentaltsev@gmail.com>
2015-05-28 22:40:27 -0400

    15) created Activity model

--- application/models/Activity.php
+++ application/models/Activity.php
@@ -14,40 +14,36 @@ class Application_Model_Activity {
 
   public function __get($name);
 
-    
+  
   public function setBalance($blc);
 
   public function getBalance();
-  
+
   
   public function setDate($text);
 
   public function getDate();
-  
+
   
   public function setDeposit($dps);
 
   public function getDeposit();
-  
 
+  
   public function setDescription($description);
 
   public function getDescription();
+
   
-    
   public function setId($id);
 
   public function getId();
-  
 
+  
   public function setWithdrawal($wtd);
 
   public function getWithdrawal();
   
-
-
-
-
 }
 
 class Application_Model_ActivityMapper {
