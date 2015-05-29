35876ada00a721eddee1ca687fafc2cac07ea96f
igoryen <igor.yentaltsev@gmail.com>
2015-05-28 21:40:08 -0400

    9) Added the Activity model

---
+++ application/models/Activity.php
@@ -0,0 +1,60 @@
+<?php
+
+// application/models/Activity.php
+class Application_Model_Activity {
+
+  protected $_balance;
+  protected $_date;
+  protected $_deposit;
+  protected $_description;
+  protected $_id;
+  protected $_withdrawal;
+
+  public function __set($name, $value);
+
+  public function __get($name);
+
+    
+  public function setBalance($blc);
+
+  public function getBalance();
+  
+  
+  public function setDate($text);
+
+  public function getDate();
+  
+  
+  public function setDeposit($dps);
+
+  public function getDeposit();
+  
+
+  public function setDescription($description);
+
+  public function getDescription();
+  
+    
+  public function setId($id);
+
+  public function getId();
+  
+
+  public function setWithdrawal($wtd);
+
+  public function getWithdrawal();
+  
+
+
+
+
+}
+
+class Application_Model_ActivityMapper {
+
+  public function save(Application_Model_Activity $activity);
+
+  public function find($id);
+
+  public function fetchAll();
+}
