012ba0742a9ec57db932b4f461f122c014c69590
igoryen <igor.e@advertiseit.com>
2015-06-17 15:39:48 -0400

    Added more assertions to the ByDescription test

--- tests/acceptance/ByDescriptionCept.php
+++ tests/acceptance/ByDescriptionCept.php
@@ -9,3 +9,18 @@ $I->wantTo('see gifts and how much was deposited');
 $I->amOnPage('/activity/description?d=gift&c=deposit');
 $I->see('gift');
 
+$I->wantTo('see all rows with the word Interest');
+$I->amOnPage('/activity/description?d=Interest&c=deposit');
+$I->see('Interest');
+
+$I->wantTo('see all the rows for the year 2000');
+$I->amOnPage('/activity/date?dt=2000');
+$I->see('2000-');
+
+$I->wantTo('see all the rows for the year 2009');
+$I->amOnPage('/activity/date?dt=2009');
+$I->see('2009-');
+
+$I->wantTo('see all the rows for the year 2004');
+$I->amOnPage('/activity/date?dt=2004');
+$I->see('2004-');
