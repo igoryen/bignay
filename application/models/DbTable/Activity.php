5375cdd2c49cb38414b584a7acfa2b3543bcdeb8
igoryen <igor.e@advertiseit.com>
2015-06-23 10:53:48 -0400

    fixed untracked files

--- application/models/DbTable/Activity.php
+++ application/models/DbTable/Activity.php
@@ -2,7 +2,7 @@
 // application/models/DbTable/Activity.php
 class Application_Model_DbTable_Activity extends Zend_Db_Table_Abstract {
 
-  protected $_schema = 'igoryen1';
+  protected $_schema = 'bignay';
   protected $_name = 'activity';
   //protected $_primary = 'id'; # the PK in the table.
   //it is an array of column names if your primary key is a compound key
